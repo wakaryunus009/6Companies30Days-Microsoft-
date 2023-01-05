d2712281b2a28368b257bf351efd6b72a414df4b
wakaryunus260 <wakaryunus009@gamil.com>
2023-01-05 22:54:26 +0530

    3rd commit

---
+++ 13.Seat Arrangement in a SpiceJet Problem - Online OA February ‘22.cpp
@@ -0,0 +1,11 @@
+//Seat Arrangement in a SpiceJet Problem - Online OA February ‘22
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+         if(n==1){
+            return 1.0;
+        }
+        else
+             return 0.5;
+    }
+};
